9266036196a05dd0b65b19238a3f542569710a95
Sergey Margelov <smargelov@gmail.com>
2020-09-26 14:37:07 +0300

    Add base layout for setting table

--- pages/settings.vue
+++ pages/settings.vue
@@ -2,11 +2,30 @@
   section
     v-card
       v-card-text
+        v-simple-table
+          template(v-slot:default)
+            thead
+              tr
+                th #
+                th Setting
+                th Value
+            tbody
+              tr
+                td 1
+                td Set dark mode
+                td
+                  v-switch(
+                    v-model="isDark"
+                  )
 </template>
 
 <script>
 export default {
-
+data () {
+  return {
+    isDark: true,
+  }
+}
 
 };
 </script>
